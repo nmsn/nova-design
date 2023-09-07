27ebec7bab17afea4c04d1a772faa99e5c32df96
nmsn <136696700@qq.com>
2023-09-07 23:41:59 +0800

    feat(components/button): 更新 button 配置

--- packages/components/button/button.tsx
+++ packages/components/button/button.tsx
@@ -1,4 +1,5 @@
 import type { SizeType, ThemeType } from "../types";
+import React from "react";
 import cx from "classnames";
 type ButtonProps = {
   type?: ThemeType | "default";
@@ -11,6 +12,7 @@ type ButtonProps = {
   disabled?: boolean;
   loading?: boolean;
   icon?: string;
+  children?: React.ReactNode;
 };
 
 const Button = ({
@@ -21,22 +23,41 @@ const Button = ({
   rounded,
   shadow,
   dashed,
+  light,
+  text,
+  children,
+  icon,
 }: ButtonProps) => {
   const isDisabled = loading || disabled;
-  // const _type = type || 'default';
-  
-  console.log(type);
+  const typeBtn = type !== "default" && !light;
+  const typeLight = type !== "default" && light;
+  const defaultLight = type === "default" && light;
+  const defaultText = type === "default" && light;
 
-  const class2 = [
+  const buttonClass = [
     "o-button-base",
-    isDisabled ? "0-disabled" : "o-hover-active-base",
+    isDisabled ? "o-disabled" : "o-hover-active-base",
     `o-${type}`,
     `o-button-${size}`,
     rounded && "rounded-full",
-    shadow ? "shadow-context:50" : "shadow-transparent",  
+    shadow ? "shadow-context:50" : "shadow-transparent",
     dashed && "border-dashed",
+    typeBtn &&
+      `o-solid ${
+        isDisabled
+          ? ""
+          : `hover-border-${type}BorderHover hover-bg-${type}LightHover hover-text-white active-text-white active-border-${type}BorderActive active-bg-${type}LightActive`
+      }`,
+    typeLight && `${isDisabled ? "o-button-light-disable" : "o-button-light"}`,
+    defaultLight ? "o-button-defaultLight" : "",
+    defaultText ? "o-button-defaultText" : "",
   ];
-  return <button className={cx(class2)}>1</button>;
+  return (
+    <button className={cx(buttonClass)}>
+      {loading && <div className="i-carbon-circle-dash animate-spin" />}
+      {children}
+    </button>
+  );
 };
 
 export default Button;
